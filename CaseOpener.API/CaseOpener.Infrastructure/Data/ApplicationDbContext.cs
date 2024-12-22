fd37f09b229541ec78ea354ba4f79b7d31c20318
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-22 19:07:01 +0200

    Created Db context class

---
+++ CaseOpener.API/CaseOpener.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CaseOpener.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {            
+        }
+    }
+}
