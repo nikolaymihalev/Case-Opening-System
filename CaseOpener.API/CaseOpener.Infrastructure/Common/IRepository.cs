51029cd0a777ed86a8d6c92172076343abd121f8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-23 13:30:30 +0200

    Created Repository interface and class

---
+++ CaseOpener.API/CaseOpener.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace CaseOpener.Infrastructure.Common
+{
+    /// <summary>
+    /// Interface for repository methods
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// All data in a table
+        /// </summary>
+        IQueryable<T> All<T>() where T : class;
+
+        /// <summary>
+        /// All data as no tracking in a table
+        /// </summary>
+        IQueryable<T> AllReadonly<T>() where T : class;
+
+        /// <summary>
+        /// Add entity to the database
+        /// </summary>
+        Task AddAsync<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Delete entity from database
+        /// </summary>
+        Task DeleteAsync<T>(object id) where T : class;
+
+        /// <summary>
+        /// Get specific entity from database by identifier
+        /// </summary>
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+        /// <summary>
+        /// Save changes in database
+        /// </summary>
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ CaseOpener.API/CaseOpener.Infrastructure/Common/Repository.cs
@@ -0,0 +1,37 @@
+﻿using CaseOpener.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseOpener.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        public DbContext Context;
+
+        private DbSet<T> DbSet<T>() where T : class => this.Context.Set<T>();
+
+        public Repository(ApplicationDbContext _context)
+        {
+            Context = _context;
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class => await this.DbSet<T>().AddAsync(entity);
+
+        public IQueryable<T> All<T>() where T : class => this.DbSet<T>().AsQueryable();
+
+        public IQueryable<T> AllReadonly<T>() where T : class => this.DbSet<T>().AsQueryable().AsNoTracking();
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+
+            if (entity != null)
+            {
+                this.DbSet<T>().Remove(entity);
+            }
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class => await this.DbSet<T>().FindAsync(id);
+
+        public async Task<int> SaveChangesAsync() => await this.Context.SaveChangesAsync();
+    }
+}
