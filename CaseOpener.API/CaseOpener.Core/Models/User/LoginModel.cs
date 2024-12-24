4694c3c57ae6d3c3898077cfb826ade8a0fa968c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-24 13:02:14 +0200

    Created dto models for User

---
+++ CaseOpener.API/CaseOpener.Core/Models/User/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using CaseOpener.Core.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace CaseOpener.Core.Models.User
+{
+    /// <summary>
+    /// Model for user login.
+    /// Contains the user's email and password required for authentication during the login process.
+    /// </summary>
+    public class LoginModel
+    {
+        /// <summary>
+        /// The user's email address used for login authentication
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessages.RequireErrorMessage)]
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// The user's password used for login authentication
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessages.RequireErrorMessage)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ CaseOpener.API/CaseOpener.Core/Models/User/UserModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CaseOpener.Core.Models.User
+{
+    /// <summary>
+    /// Model for user information
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// Unique identifier for the user.
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Username chosen by the user.
+        /// </summary>
+        public string Username { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email address of the user.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Balance of the user.
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// Date and time the user joined.
+        /// </summary>
+        public DateTime DateJoined { get; set; }
+    }
+}
