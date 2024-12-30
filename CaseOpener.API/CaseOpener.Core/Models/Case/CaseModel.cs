fe23577edf4707eaf78aaf085b1013fc1c643ae4
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-30 14:34:55 +0200

    Created models for information for Case and Item

---
+++ CaseOpener.API/CaseOpener.Core/Models/Case/CaseModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CaseOpener.Core.Models.Case
+{
+    /// <summary>
+    /// Model for information about case
+    /// </summary>
+    public class CaseModel
+    {
+        /// <summary>
+        /// Unique identifier for the case.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the case.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Image of the case.
+        /// </summary>
+        public string Image { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Price of the case.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Items in the case.
+        /// </summary>
+        public List<ItemModel> Items { get; set; } = new List<ItemModel>();
+    }
+}
