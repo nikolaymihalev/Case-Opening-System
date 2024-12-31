a327289fa8a6989b0c7a55863b3723c62a8cdb4d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-31 10:38:22 +0200

    Created Case models

---
+++ CaseOpener.API/CaseOpener.Core/Models/Case/CaseFormModel.cs
@@ -0,0 +1,50 @@
+﻿using CaseOpener.Core.Constants;
+using CaseOpener.Infrastructure.Constants;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace CaseOpener.Core.Models.Case
+{
+    /// <summary>
+    /// Model for adding or editing Case
+    /// </summary>
+    public class CaseFormModel
+    {
+        /// <summary>
+        /// Unique identifier for the case.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the case.
+        /// </summary>
+        [Required(ErrorMessage = ReturnMessages.REQUIRED)]
+        [StringLength(
+            PropertiesConstants.CASE_NAME_MAX_LENGTH, 
+            MinimumLength = PropertiesConstants.CASE_NAME_MIN_LENGTH, 
+            ErrorMessage = ReturnMessages.STRING_LENGTH)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Uploaded file for image
+        /// </summary>
+        public IFormFile? ImageFile { get; set; }
+
+        /// <summary>
+        /// Image of the case.
+        /// </summary>
+        [Required(ErrorMessage = ReturnMessages.REQUIRED)]
+        public byte[] Image { get; set; } = new byte[128];
+
+        /// <summary>
+        /// Price of the case.
+        /// </summary>
+        [Required(ErrorMessage = ReturnMessages.REQUIRED)]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Items in the case.
+        /// </summary>
+        public int[] Items { get; set; } = new int[0];
+    }
+}
---
+++ CaseOpener.API/CaseOpener.Core/Models/Case/DailyRewardModel.cs
@@ -0,0 +1,28 @@
+﻿namespace CaseOpener.Core.Models.Case
+{
+    /// <summary>
+    /// Model for daily reward
+    /// </summary>
+    public class DailyRewardModel
+    {
+        /// <summary>
+        /// Unique identifier for the reward.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Unique identifier for the user.
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Unique identifier for the case.
+        /// </summary>
+        public int CaseId { get; set; }
+
+        /// <summary>
+        /// The last claimed date of the daily reward.
+        /// </summary>
+        public DateTime LastClaimedDate { get; set; }
+    }
+}
