19f468fcf234dd5f00674bbf9217e10f8a1da169
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-06 10:57:26 +0200

    Added tests for Admin service

---
+++ CaseOpener.API/CaseOpener.UnitTests/InMemoryDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using CaseOpener.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseOpener.UnitTests
+{
+    public static class InMemoryDbContextFactory
+    {
+        public static ApplicationDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()) // Уникална база данни за всеки тест
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+
+}
