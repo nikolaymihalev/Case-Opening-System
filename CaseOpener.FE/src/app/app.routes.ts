c72c162b53e58be70e9592b011f85b3eb0ef85d2
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-08 14:46:50 +0200

    Created login page

--- CaseOpener.FE/src/app/app.routes.ts
+++ CaseOpener.FE/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+];
