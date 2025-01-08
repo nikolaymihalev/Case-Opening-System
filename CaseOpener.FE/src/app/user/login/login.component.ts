cddf5c3802553df957d2f64ad28b6265c8dd1d33
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-08 15:50:52 +0200

    Added validation in login and register forms

--- CaseOpener.FE/src/app/user/login/login.component.ts
+++ CaseOpener.FE/src/app/user/login/login.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { EmailDirective } from '../../directives/emai.directive';
+import { UserValidationConstants } from '../constants/user.validation.constants';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, EmailDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  passwordMinLength = UserValidationConstants.PASSWORD_MIN_LENGTH;
+  passwordMaxLength = UserValidationConstants.PASSWORD_MAX_LENGTH;
 
+  emailMinLength = UserValidationConstants.EMAIL_MIN_LENGTH;
+  emailMaxLength = UserValidationConstants.EMAIL_MAX_LENGTH;
 }
--- CaseOpener.FE/src/app/user/register/register.component.ts
+++ CaseOpener.FE/src/app/user/register/register.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { UserValidationConstants } from '../constants/user.validation.constants';
+import { EmailDirective } from '../../directives/emai.directive';
+import { MatchPasswordDirective } from '../../directives/match.passwords.directive';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, EmailDirective, MatchPasswordDirective],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  passwordMinLength = UserValidationConstants.PASSWORD_MIN_LENGTH;
+  passwordMaxLength = UserValidationConstants.PASSWORD_MAX_LENGTH;
 
+  emailMinLength = UserValidationConstants.EMAIL_MIN_LENGTH;
+  emailMaxLength = UserValidationConstants.EMAIL_MAX_LENGTH;
+
+  usernameMinLength = UserValidationConstants.USERNAME_MIN_LENGTH;
+  usernameMaxLength = UserValidationConstants.USERNAME_MAX_LENGTH;
 }
