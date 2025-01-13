4c0d0549d3242f7a22633712df49530126056cc1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-13 11:08:53 +0200

    Created notification component with service

---
+++ CaseOpener.FE/src/app/shared/notification/notification.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css'
+})
+export class NotificationComponent implements OnInit{
+  isVisible: boolean = false;  
+  
+  @Input() message: string = '';
+  @Input() type: string = 'success';
+
+  ngOnInit(): void {
+    this.showNotification();
+  }
+
+  showNotification(){
+    this.isVisible = true;
+    setTimeout(()=>{
+      this.isVisible = false;
+    }, 2000);
+  }
+}
