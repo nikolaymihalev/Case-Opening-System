50c66a5ec6fd043ed74a4ac70e2218544f21aa00
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-14 10:22:56 +0200

    Created user profile and opened cases components

--- CaseOpener.FE/src/app/app.routes.ts
+++ CaseOpener.FE/src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { MainComponent } from './main/main.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'history', loadComponent: () =>
+        import('./case/opened-cases/opened-cases.component').then(
+        (c) => c.OpenedCasesComponent), canActivate: [AuthGuard],
+    },
+    { path: 'settings', loadComponent: () =>
+        import('./user/profile/profile.component').then(
+        (c) => c.ProfileComponent), canActivate: [AuthGuard],
+    },
 ];
---
+++ CaseOpener.FE/src/app/case/opened-cases/opened-cases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opened-cases',
+  standalone: true,
+  imports: [],
+  templateUrl: './opened-cases.component.html',
+  styleUrl: './opened-cases.component.css'
+})
+export class OpenedCasesComponent {
+
+}
