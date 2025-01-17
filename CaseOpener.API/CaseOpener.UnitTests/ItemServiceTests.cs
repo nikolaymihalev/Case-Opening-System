99066d2711801fdf3d1721cecf1b2d0261d22702
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-17 12:06:29 +0200

    Updated item enums in api

--- CaseOpener.API/CaseOpener.UnitTests/ItemServiceTests.cs
+++ CaseOpener.API/CaseOpener.UnitTests/ItemServiceTests.cs
@@ -36,7 +36,7 @@ namespace CaseOpener.UnitTests
             {
                 Name = "Test Item",
                 Type = "Skin",
-                Rarity = "Industrial",
+                Rarity = "MilSpec",
                 ImageUrl = "http://example.com/item.png",
                 Amount = 5
             };
