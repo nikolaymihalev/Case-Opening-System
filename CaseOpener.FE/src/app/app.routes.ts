e088222722042f69ebaf0e0a46d9dd6bf963e630
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-22 15:22:01 +0200

    Created error component

--- CaseOpener.FE/src/app/app.routes.ts
+++ CaseOpener.FE/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { MainComponent } from './main/main.component';
 import { AuthGuard } from './guards/auth.guard';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -34,5 +35,7 @@ export const routes: Routes = [
     { path: 'mycases', loadComponent: () =>
         import('./case/bought-cases/bought-cases.component').then(
         (c) => c.BoughtCasesComponent), canActivate: [AuthGuard],
-    },
+    }, 
+    { path: '404', component: ErrorComponent },
+    { path: '**', redirectTo: '/404' },
 ];
